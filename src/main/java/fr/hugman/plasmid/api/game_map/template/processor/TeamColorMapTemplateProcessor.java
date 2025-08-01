73b08978a86d9bb4706f24096477cc92042eaf7e
Hugman <contact.hugman@gmail.com>
2025-08-01 23:39:01 +0200

    Add replace block entities template processor

--- src/main/java/fr/hugman/plasmid/api/game_map/template/processor/TeamColorMapTemplateProcessor.java
+++ src/main/java/fr/hugman/plasmid/api/game_map/template/processor/TeamColorMapTemplateProcessor.java
@@ -3,8 +3,11 @@ package fr.hugman.plasmid.api.game_map.template.processor;
 import com.mojang.serialization.MapCodec;
 import com.mojang.serialization.codecs.RecordCodecBuilder;
 import fr.hugman.plasmid.api.game.attachment.PlasmidGameAttachments;
+import fr.hugman.plasmid.api.util.ColoredItems;
 import net.minecraft.block.Block;
 import net.minecraft.block.BlockState;
+import net.minecraft.item.Item;
+import net.minecraft.registry.Registries;
 import net.minecraft.state.property.Property;
 import net.minecraft.text.Text;
 import net.minecraft.util.DyeColor;
@@ -25,70 +28,39 @@ public record TeamColorMapTemplateProcessor(List<DyeColor> baseColors) implement
     }
 
     public void processTemplate(GameActivity activity, MapTemplate template) {
-        var teamList = activity.getGameSpace().getAttachment(PlasmidGameAttachments.TEAM_LIST).list();
+        var teamList = activity.getGameSpace().getAttachment(PlasmidGameAttachments.TEAM_LIST).list(); //TODO: throw error if no team list is present
 
         if (teamList.size() > this.baseColors.size()) {
             throw new GameOpenException(Text.literal("Not enough base colors provided for the number of teams.")); //TODO: translate
         }
 
-        var colorMap = new HashMap<DyeColor, DyeColor>();
+        var blockMap = new HashMap<Block, Block>();
+        var blockEntityReplace = new HashMap<String, String>();
         for (int i = 0; i < this.baseColors.size(); i++) {
-            colorMap.put(this.baseColors.get(i), teamList.get(i).config().blockDyeColor());
+            var baseColor = this.baseColors.get(i);
+            var teamColor = teamList.get(i).config().blockDyeColor();
+            blockMap.put(ColoredBlocks.wool(baseColor), ColoredBlocks.wool(teamColor));
+            blockMap.put(ColoredBlocks.carpet(baseColor), ColoredBlocks.carpet(teamColor));
+            blockMap.put(ColoredBlocks.terracotta(baseColor), ColoredBlocks.terracotta(teamColor));
+            blockMap.put(ColoredBlocks.glazedTerracotta(baseColor), ColoredBlocks.glazedTerracotta(teamColor));
+            blockMap.put(ColoredBlocks.concrete(baseColor), ColoredBlocks.concrete(teamColor));
+            blockMap.put(ColoredBlocks.concretePowder(baseColor), ColoredBlocks.concretePowder(teamColor));
+            blockMap.put(ColoredBlocks.glass(baseColor), ColoredBlocks.glass(teamColor));
+            blockMap.put(ColoredBlocks.glassPane(baseColor), ColoredBlocks.glassPane(teamColor));
+            blockMap.put(ColoredBlocks.bed(baseColor), ColoredBlocks.bed(teamColor));
+            blockMap.put(ColoredBlocks.shulkerBox(baseColor), ColoredBlocks.shulkerBox(teamColor));
+            blockMap.put(ColoredBlocks.candle(baseColor), ColoredBlocks.candle(teamColor));
+            blockMap.put(ColoredBlocks.candleCake(baseColor), ColoredBlocks.candleCake(teamColor));
+            blockEntityReplace.put(Registries.ITEM.getId(ColoredItems.dye(baseColor)).toString(), Registries.ITEM.getId(ColoredItems.dye(teamColor)).toString());
+            blockEntityReplace.put(Registries.ITEM.getId(ColoredItems.bundle(baseColor)).toString(), Registries.ITEM.getId(ColoredItems.bundle(teamColor)).toString());
+            blockEntityReplace.put(Registries.ITEM.getId(ColoredItems.harness(baseColor)).toString(), Registries.ITEM.getId(ColoredItems.harness(teamColor)).toString());
         }
 
-        template.getBounds().forEach(pos -> {
-            var state = template.getBlockState(pos);
-            var block = state.getBlock();
-            Block newBlock = null;
-            for (var entry : colorMap.entrySet()) {
-                var baseColor = entry.getKey();
-                var targetColor = entry.getValue();
-                if (ColoredBlocks.wool(baseColor) == block) {
-                    newBlock = ColoredBlocks.wool(targetColor);
-                    break;
-                } else if (ColoredBlocks.carpet(baseColor) == block) {
-                    newBlock = ColoredBlocks.carpet(targetColor);
-                    break;
-                } else if (ColoredBlocks.terracotta(baseColor) == block) {
-                    newBlock = ColoredBlocks.terracotta(targetColor);
-                    break;
-                } else if (ColoredBlocks.glazedTerracotta(baseColor) == block) {
-                    newBlock = ColoredBlocks.glazedTerracotta(targetColor);
-                    break;
-                } else if (ColoredBlocks.concrete(baseColor) == block) {
-                    newBlock = ColoredBlocks.concrete(targetColor);
-                    break;
+        new ReplaceBlocksTemplateProcessor(blockMap).processTemplate(activity, template);
 
-                } else if (ColoredBlocks.concretePowder(baseColor) == block) {
-                    newBlock = ColoredBlocks.concretePowder(targetColor);
-                    break;
-                } else if (ColoredBlocks.glass(baseColor) == block) {
-                    newBlock = ColoredBlocks.glass(targetColor);
-                    break;
-                } else if (ColoredBlocks.glassPane(baseColor) == block) {
-                    newBlock = ColoredBlocks.glassPane(targetColor);
-                    break;
-                } else if (ColoredBlocks.bed(baseColor) == block) {
-                    newBlock = ColoredBlocks.bed(targetColor);
-                    break;
-                } else if (ColoredBlocks.shulkerBox(baseColor) == block) {
-                    newBlock = ColoredBlocks.shulkerBox(targetColor);
-                    break;
-                } else if (ColoredBlocks.candle(baseColor) == block) {
-                    newBlock = ColoredBlocks.candle(targetColor);
-                    break;
-                } else if (ColoredBlocks.candleCake(baseColor) == block) {
-                    newBlock = ColoredBlocks.candleCake(targetColor);
-                    break;
-                }
-            }
-            if (newBlock != null) {
-                BlockState newState = newBlock.getDefaultState();
-                for (Property property : state.getProperties()) {
-                    newState = newState.contains(property) ? newState.with(property, state.get(property)) : newState;
-                }
-                template.setBlockState(pos, newState);
-            }
-        });
+        for(var entry : blockMap.entrySet()) {
+            blockEntityReplace.put(Registries.BLOCK.getId(entry.getKey()).toString(), Registries.BLOCK.getId(entry.getValue()).toString());
+        }
+        new ReplaceBlockEntitiesTemplateProcessor(blockEntityReplace).processTemplate(activity, template);
     }
 }
